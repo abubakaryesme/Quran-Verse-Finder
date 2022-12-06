b2982f72b5b7852aa117911e5d57c19cc2cc856c
abubakaryesme <abubakerakmal2@gmail.com>
2022-12-07 01:46:27 +0500

    Input Validation Done!

--- app/src/main/java/com/example/holyquran/MainActivity.java
+++ app/src/main/java/com/example/holyquran/MainActivity.java
@@ -77,6 +77,7 @@ public class MainActivity extends AppCompatActivity {
                     verseText.setText("");
                     surahNameUrdu.setText("_____");
                     surahNameEng.setText("______");
+                    verseNumber=surahNumber=-1;
                 }
                 else
                 {
